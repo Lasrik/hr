7faeefa00b5ac2067a0c39e9f20304ec07ee751c
Tobias Dittrich <dittrich@alternate.de>
2016-09-16 17:20:30 +0200

    gitignore

--- src/main/java/de/tle/hr/packmandfs/Solution.java
+++ src/main/java/de/tle/hr/packmandfs/Solution.java
@@ -1,11 +1,56 @@
 package de.tle.hr.packmandfs;
 
+import java.awt.*;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Solution {
 
-  static void dfs(int r, int c, int pacman_r, int pacman_c, int food_r, int food_c, String[] grid) {
-    //Your logic here
+  static void dfs(int rows, int cols, int pacman_r, int pacman_c, int food_r, int food_c, char[][] grid) {
+    Stack<Point> stack = new Stack<>();
+    boolean[][] visited = new boolean[rows][cols];
+    stack.push(new Point(pacman_r, pacman_c));
+
+    int number = 0;
+
+    while (!stack.empty()) {
+      Point current = stack.pop();
+      visited[current.x][current.y] = true;
+      System.out.println(current.x + " " + current.y);
+      number++;
+      if (grid[current.x][current.y] == '.') {
+        System.out.println(number);
+        return;
+      }
+
+      // UP
+      if (current.x > 0) {
+        if (!visited[current.x - 1][current.y] && grid[current.x - 1][current.y] != '%') {
+          stack.push(new Point(current.x - 1, current.y));
+        }
+      }
+
+      // LEFT
+      if (current.y > 0) {
+        if (!visited[current.x][current.y - 1] && grid[current.x][current.y - 1] != '%') {
+          stack.push(new Point(current.x, current.y - 1));
+        }
+      }
+
+      // RIGHT
+      if (current.y + 1 < cols) {
+        if (!visited[current.x][current.y + 1] && grid[current.x][current.y + 1] != '%') {
+          stack.push(new Point(current.x, current.y + 1));
+        }
+      }
+
+      // DOWN
+      if (current.x + 1 < rows) {
+        if (!visited[current.x + 1][current.y] && grid[current.x + 1][current.y] != '%') {
+          stack.push(new Point(current.x + 1, current.y));
+        }
+      }
+    }
   }
 
   public static void main(String[] args) {
@@ -18,15 +63,18 @@ public class Solution {
     int food_r = in.nextInt();
     int food_c = in.nextInt();
 
-    int r = in.nextInt();
-    int c = in.nextInt();
+    int rows = in.nextInt();
+    int cols = in.nextInt();
 
-    String grid[] = new String[r];
+    char[][] grid = new char[rows][cols];
 
-    for (int i = 0; i < r; i++) {
-      grid[i] = in.next();
+    for (int i = 0; i < rows; i++) {
+      String line = in.next();
+      for (int j = 0; j < line.length(); j++) {
+        grid[i][j] = line.charAt(j);
+      }
     }
 
-    dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid);
+    dfs(rows, cols, pacman_r, pacman_c, food_r, food_c, grid);
   }
 }
